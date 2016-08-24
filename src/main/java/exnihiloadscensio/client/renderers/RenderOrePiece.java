f7f15db8598e627a280f3501948050713488f5f9
Michael Lydeamore <michael.lydeamore@adelaide.edu.au>
2016-08-24 19:39:09 +1000

    I'm bad at version contorl

---
+++ src/main/java/exnihiloadscensio/client/renderers/RenderOrePiece.java
@@ -0,0 +1,13 @@
+package exnihiloadscensio.client.renderers;
+
+import net.minecraft.client.renderer.color.IItemColor;
+import net.minecraft.item.ItemStack;
+
+public class RenderOrePiece implements IItemColor {
+
+	@Override
+	public int getColorFromItemstack(ItemStack stack, int tintIndex) {
+		return 0;
+	}
+
+}
