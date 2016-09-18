422dbed8b220ea0b9d3dfdabeeb19c097518cfb8
Michael Lydeamore <mlydeamore@student.unimelb.edu.au>
2016-09-18 12:51:57 +1000

    Add the other vanilla leaves for crooking

--- src/main/java/exnihiloadscensio/registries/CrookRegistry.java
+++ src/main/java/exnihiloadscensio/registries/CrookRegistry.java
@@ -13,14 +13,14 @@ import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 
 public class CrookRegistry {
-	
+
 	private static HashMap<BlockInfo, ArrayList<CrookReward>> map = new HashMap<BlockInfo, ArrayList<CrookReward>>();
-	
+
 	public static void addCrookRecipe(IBlockState state, ItemStack reward, float chance, float fortuneChance) {
 		BlockInfo info = new BlockInfo(state);
 		addCrookRecipe(info, reward, chance, fortuneChance);
 	}
-	
+
 	public static void addCrookRecipe(BlockInfo info, ItemStack reward, float chance, float fortuneChance) {
 		ArrayList<CrookReward> list = map.get(info);
 		if (list == null)
@@ -30,24 +30,26 @@ public class CrookRegistry {
 		list.add(new CrookReward(reward, chance, fortuneChance));
 		map.put(info, list);
 	}
-	
+
 	public static boolean registered(Block block)
 	{
 		return map.containsKey(new BlockInfo(block.getDefaultState()));
 	}
-	
+
 	public static ArrayList<CrookReward> getRewards(IBlockState state)
 	{
 		BlockInfo info = new BlockInfo(state);
 		if (!map.containsKey(info))
 			return null;
-		
+
 		return map.get(info);
 	}
-	
+
 	public static void registerDefaults() {
-		for (int i = 0 ; i < 16 ; i++)
+		for (int i = 0 ; i < 16 ; i++) {
 			addCrookRecipe(new BlockInfo(Blocks.LEAVES, i), ItemResource.getResourceStack(ItemResource.SILKWORM), 0.1f, 0f);
+			addCrookRecipe(new BlockInfo(Blocks.LEAVES2, i), ItemResource.getResourceStack(ItemResource.SILKWORM), 0.1f, 0f);
+		}
 	}
 
 }
