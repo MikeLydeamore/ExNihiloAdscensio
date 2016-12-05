1a08ad598acbb630140c2156d0a98f1d820c0c19
Michael Lydeamore <mlydeamore@student.unimelb.edu.au>
2016-12-05 14:38:36 +1100

    Add Tinkers Melting compat

--- src/main/java/exnihiloadscensio/compatibility/tconstruct/CompatTConstruct.java
+++ src/main/java/exnihiloadscensio/compatibility/tconstruct/CompatTConstruct.java
@@ -1,7 +1,13 @@
 package exnihiloadscensio.compatibility.tconstruct;
 
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
 import exnihiloadscensio.items.ENItems;
+import exnihiloadscensio.items.ore.ItemOre;
+import exnihiloadscensio.registries.OreRegistry;
 import slimeknights.tconstruct.library.TinkerRegistry;
+import slimeknights.tconstruct.library.materials.Material;
 import slimeknights.tconstruct.library.modifiers.Modifier;
 
 public class CompatTConstruct
@@ -12,5 +18,16 @@ public class CompatTConstruct
         
         TinkerRegistry.registerModifier(smashingModifier);
         smashingModifier.addItem(ENItems.hammerDiamond);
+        
+        registerMelting();
+    }
+    
+    public static void registerMelting() {
+    	for (ItemOre ore : OreRegistry.getItemOreRegistry()) {
+    		if (FluidRegistry.isFluidRegistered(ore.getOre().getName())) {
+    			Fluid fluid = FluidRegistry.getFluid(ore.getOre().getName());
+    			TinkerRegistry.registerMelting(new ItemStack(ore, 1, 1), fluid, 2*Material.VALUE_Ingot);
+    		}
+    	}
     }
 }
