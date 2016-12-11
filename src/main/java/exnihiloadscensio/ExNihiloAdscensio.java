42ca78d06ac9c0d1dd59b4581009db7e4f1caa2c
Michael Lydeamore <games.freak41@gmail.com>
2016-12-12 09:04:17 +1100

    Run formatter

--- src/main/java/exnihiloadscensio/ExNihiloAdscensio.java
+++ src/main/java/exnihiloadscensio/ExNihiloAdscensio.java
@@ -44,37 +44,35 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-@Mod(modid = ExNihiloAdscensio.MODID, name="Ex Nihilo Adscensio", version="0.1.5")
+@Mod(modid = ExNihiloAdscensio.MODID, name = "Ex Nihilo Adscensio", version = "0.1.5")
 public class ExNihiloAdscensio {
 
 	public static final String MODID = "exnihiloadscensio";
 
-	@SidedProxy(serverSide="exnihiloadscensio.CommonProxy",clientSide="exnihiloadscensio.client.ClientProxy")
+	@SidedProxy(serverSide = "exnihiloadscensio.CommonProxy", clientSide = "exnihiloadscensio.client.ClientProxy")
 	public static CommonProxy proxy;
-	
+
 	@Instance(MODID)
 	public static ExNihiloAdscensio instance;
 
 	public static File configDirectory;
-	
+
 	public static boolean configsLoaded = false;
-	
-	static
-	{
+
+	static {
 		FluidRegistry.enableUniversalBucket();
 	}
-	
+
 	@EventHandler
-	public static void preInit(FMLPreInitializationEvent event)
-	{
-	    LogUtil.setup();
-	    
+	public static void preInit(FMLPreInitializationEvent event) {
+		LogUtil.setup();
+
 		configDirectory = new File(event.getModConfigurationDirectory(), "exnihiloadscensio");
 		configDirectory.mkdirs();
-		
+
 		Config.doNormalConfig(new File(configDirectory, "ExNihiloAdscensio.cfg"));
-		
-        ENCapabilities.init();
+
+		ENCapabilities.init();
 		ENItems.init();
 		ENBlocks.init();
 		ENEntities.init();
@@ -86,8 +84,7 @@ public class ExNihiloAdscensio {
 
 		MinecraftForge.EVENT_BUS.register(new HandlerCrook());
 
-		if (Config.enableBarrels)
-		{
+		if (Config.enableBarrels) {
 			BarrelModeRegistry.registerDefaults();
 		}
 
@@ -95,70 +92,61 @@ public class ExNihiloAdscensio {
 	}
 
 	@EventHandler
-	public static void init(FMLInitializationEvent event)
-	{
+	public static void init(FMLInitializationEvent event) {
 		OreRegistry.loadJson(new File(configDirectory, "OreRegistry.json"));
 		loadConfigs();
-	    
+
 		Recipes.init();
-        OreRegistry.doRecipes();
-		
+		OreRegistry.doRecipes();
+
 		proxy.initOreModels();
 		proxy.registerColorHandlers();
-		
-		
-		
-		FMLInterModComms.sendMessage("Waila", "register", "exnihiloadscensio.compatibility.CompatWaila.callbackRegister");
+
+		FMLInterModComms.sendMessage("Waila", "register",
+				"exnihiloadscensio.compatibility.CompatWaila.callbackRegister");
 	}
 
 	@EventHandler
-	public static void postInit(FMLPostInitializationEvent event)
-	{
-        
-		if(Loader.isModLoaded("tconstruct"))
-		{
+	public static void postInit(FMLPostInitializationEvent event) {
+
+		if (Loader.isModLoaded("tconstruct")) {
 			CompatTConstruct.postInit();
 		}
 	}
-	
-	public static void loadConfigs()
-	{
-	    configsLoaded = true;
-	    
-        CompostRegistry.loadJson(new File(configDirectory, "CompostRegistry.json"));
-        CompostRegistry.recommendAllFood(new File(configDirectory, "RecommendedFoodRegistry.json"));
-        HammerRegistry.loadJson(new File(configDirectory, "HammerRegistry.json"));
-        FluidBlockTransformerRegistry.loadJson(new File(configDirectory, "FluidBlockTransformerRegistry.json"));
-        FluidOnTopRegistry.loadJson(new File(configDirectory, "FluidOnTopRegistry.json"));
-        HeatRegistry.loadJson(new File(configDirectory, "HeatRegistry.json"));
-        CrucibleRegistry.loadJson(new File(configDirectory, "CrucibleRegistry.json"));
-        SieveRegistry.loadJson(new File(configDirectory, "SieveRegistry.json"));
-        CrookRegistry.loadJson(new File(configDirectory, "CrookRegistry.json"));
-        FluidTransformRegistry.loadJson(new File(configDirectory, "FluidTransformRegistry.json"));
-        BarrelLiquidBlacklistRegistry.loadJson(new File(configDirectory, "BarrelLiquidBlacklistRegistry.json"));
-        
-	}
-	
-	@EventHandler
-	public static void serverStart(FMLServerStartingEvent event)
-	{
-	    event.registerServerCommand(new CommandReloadConfig());
+
+	public static void loadConfigs() {
+		configsLoaded = true;
+
+		CompostRegistry.loadJson(new File(configDirectory, "CompostRegistry.json"));
+		CompostRegistry.recommendAllFood(new File(configDirectory, "RecommendedFoodRegistry.json"));
+		HammerRegistry.loadJson(new File(configDirectory, "HammerRegistry.json"));
+		FluidBlockTransformerRegistry.loadJson(new File(configDirectory, "FluidBlockTransformerRegistry.json"));
+		FluidOnTopRegistry.loadJson(new File(configDirectory, "FluidOnTopRegistry.json"));
+		HeatRegistry.loadJson(new File(configDirectory, "HeatRegistry.json"));
+		CrucibleRegistry.loadJson(new File(configDirectory, "CrucibleRegistry.json"));
+		SieveRegistry.loadJson(new File(configDirectory, "SieveRegistry.json"));
+		CrookRegistry.loadJson(new File(configDirectory, "CrookRegistry.json"));
+		FluidTransformRegistry.loadJson(new File(configDirectory, "FluidTransformRegistry.json"));
+		BarrelLiquidBlacklistRegistry.loadJson(new File(configDirectory, "BarrelLiquidBlacklistRegistry.json"));
+
 	}
 
-	public static CreativeTabs tabExNihilo = new CreativeTabs("exNihilo")
-	{
+	@EventHandler
+	public static void serverStart(FMLServerStartingEvent event) {
+		event.registerServerCommand(new CommandReloadConfig());
+	}
+
+	public static CreativeTabs tabExNihilo = new CreativeTabs("exNihilo") {
 		@Override
 		@SideOnly(Side.CLIENT)
-		public Item getTabIconItem()
-		{
+		public Item getTabIconItem() {
 			return Item.getItemFromBlock(ENBlocks.sieve);
 		}
 	};
 
 	@EventHandler
 	public static void modMapping(FMLModIdMappingEvent event) {
-		
-	}
 
+	}
 
 }
