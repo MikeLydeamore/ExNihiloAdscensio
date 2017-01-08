6d48cf9285e74ac1bb992ff92bca878169e79f1a
Michael Lydeamore <mlydeamore@student.unimelb.edu.au>
2017-01-09 09:14:22 +1100

    Run formatter

--- src/main/java/exnihiloadscensio/barrel/modes/transform/BarrelModeFluidTransform.java
+++ src/main/java/exnihiloadscensio/barrel/modes/transform/BarrelModeFluidTransform.java
@@ -11,7 +11,6 @@ import exnihiloadscensio.registries.types.FluidTransformer;
 import exnihiloadscensio.texturing.Color;
 import exnihiloadscensio.tiles.TileBarrel;
 import exnihiloadscensio.util.BlockInfo;
-import exnihiloadscensio.util.LogUtil;
 import exnihiloadscensio.util.Util;
 import lombok.Getter;
 import lombok.Setter;
@@ -31,11 +30,13 @@ import net.minecraftforge.items.ItemStackHandler;
 
 public class BarrelModeFluidTransform implements IBarrelMode {
 
-	@Setter @Getter
+	@Setter
+	@Getter
 	private FluidStack inputStack, outputStack;
 	@Getter
 	private float progress = 0;
-	@Setter @Getter
+	@Setter
+	@Getter
 	private FluidTransformer transformer;
 
 	@Override
@@ -80,30 +81,27 @@ public class BarrelModeFluidTransform implements IBarrelMode {
 	}
 
 	@Override
-	public boolean onBlockActivated(World world, TileBarrel barrel, BlockPos pos, IBlockState state,
-			EntityPlayer player, EnumFacing side, float hitX, float hitY, float hitZ) {
+	public boolean onBlockActivated(World world, TileBarrel barrel,
+			BlockPos pos, IBlockState state, EntityPlayer player,
+			EnumFacing side, float hitX, float hitY, float hitZ) {
 		return false;
 	}
-    
-    @Override
-    public TextureAtlasSprite getTextureForRender(TileBarrel barrel)
-    {
-        if (progress < 0.5)
-        {
-            return Util.getTextureFromFluidStack(inputStack);
-        }
-        else
-        {
-            return Util.getTextureFromFluidStack(outputStack);
-        }
-    }
-    
-    @Override
-    public Color getColorForRender()
-    {
-        return Color.average(Util.blackColor, Util.whiteColor, 2 * Math.abs(progress - 0.5F));
-    }
-    
+
+	@Override
+	public TextureAtlasSprite getTextureForRender(TileBarrel barrel) {
+		if (progress < 0.5) {
+			return Util.getTextureFromFluidStack(inputStack);
+		} else {
+			return Util.getTextureFromFluidStack(outputStack);
+		}
+	}
+
+	@Override
+	public Color getColorForRender() {
+		return Color.average(Util.blackColor, Util.whiteColor,
+				2 * Math.abs(progress - 0.5F));
+	}
+
 	@Override
 	public float getFilledLevelForRender(TileBarrel barrel) {
 		return 1;
@@ -112,28 +110,44 @@ public class BarrelModeFluidTransform implements IBarrelMode {
 	@Override
 	public void update(TileBarrel barrel) {
 		if (transformer == null) {
-			transformer = FluidTransformRegistry.getFluidTransformer(inputStack.getFluid().getName(), outputStack.getFluid().getName());
+			transformer = FluidTransformRegistry.getFluidTransformer(inputStack
+					.getFluid().getName(), outputStack.getFluid().getName());
 		}
 		if (transformer == null)
 			return;
 		if (progress < 1) {
-			int numberOfBlocks = Util.getNumSurroundingBlocksAtLeastOneOf(transformer.getTransformingBlocks(), barrel.getPos().add(0, -1, 0), barrel.getWorld())
-					+ Util.getNumSurroundingBlocksAtLeastOneOf(transformer.getTransformingBlocks(), barrel.getPos(), barrel.getWorld());
+			int numberOfBlocks = Util.getNumSurroundingBlocksAtLeastOneOf(
+					transformer.getTransformingBlocks(),
+					barrel.getPos().add(0, -1, 0), barrel.getWorld())
+					+ Util.getNumSurroundingBlocksAtLeastOneOf(
+							transformer.getTransformingBlocks(),
+							barrel.getPos(), barrel.getWorld());
 			if (numberOfBlocks > 0) {
 				progress += numberOfBlocks * 1.0 / transformer.getDuration();
-				
+
 				if (barrel.getWorld().rand.nextDouble() < 0.005) {
 					boolean spawned = false;
-					ArrayList<BlockInfo> blockList = new ArrayList<BlockInfo>(Arrays.asList(transformer.getTransformingBlocks()));
-					for (int xShift = -1 ; xShift <= 1 ; xShift++) {
-						for (int zShift = -1 ; zShift <= 1 ; zShift++) {
+					ArrayList<BlockInfo> blockList = new ArrayList<BlockInfo>(
+							Arrays.asList(transformer.getTransformingBlocks()));
+					for (int xShift = -1; xShift <= 1; xShift++) {
+						for (int zShift = -1; zShift <= 1; zShift++) {
 							if (!spawned) {
-								BlockPos testPos = barrel.getPos().add(xShift, -1, zShift);
-								if (blockList.contains(new BlockInfo(barrel.getWorld().getBlockState(testPos)))
-										&& barrel.getWorld().isAirBlock(testPos.add(0, 1, 0))) {
-									BlockInfo[] toSpawn = transformer.getBlocksToSpawn();
+								BlockPos testPos = barrel.getPos().add(xShift,
+										-1, zShift);
+								if (blockList.contains(new BlockInfo(barrel
+										.getWorld().getBlockState(testPos)))
+										&& barrel.getWorld().isAirBlock(
+												testPos.add(0, 1, 0))) {
+									BlockInfo[] toSpawn = transformer
+											.getBlocksToSpawn();
 									if (toSpawn != null && toSpawn.length > 0) {
-										barrel.getWorld().setBlockState(testPos.add(0, 1, 0), toSpawn[barrel.getWorld().rand.nextInt(toSpawn.length)].getBlockState());
+										barrel.getWorld()
+												.setBlockState(
+														testPos.add(0, 1, 0),
+														toSpawn[barrel
+																.getWorld().rand
+																.nextInt(toSpawn.length)]
+																.getBlockState());
 										spawned = true;
 									}
 								}
@@ -175,8 +189,9 @@ public class BarrelModeFluidTransform implements IBarrelMode {
 	}
 
 	@Override
-	public List<String> getWailaTooltip(TileBarrel barrel, List<String> currenttip) {
-		currenttip.add("Transforming: "+Math.round(progress*100)+"%");
+	public List<String> getWailaTooltip(TileBarrel barrel,
+			List<String> currenttip) {
+		currenttip.add("Transforming: " + Math.round(progress * 100) + "%");
 		return currenttip;
 	}
 
